6db7fcbc1c8ee75983c09c94f7ef866e17402472
Jyothi Sumer <mjyothisumer@gmail.com>
2022-09-05 22:11:55 -0400

    Wrapper for updating cli options

--- cli.py
+++ cli.py
@@ -1,9 +1,10 @@
 import sys
 
-for idx, arg in enumerate(sys.argv):
-    for key, value in arguments.items():
-        if((arg == "--" + key) or (arg == "-" + key[0:1])):
-            arguments[key] = coerce(sys.argv[idx+1])
+def updateCli(arguments):
+    for idx, arg in enumerate(sys.argv):
+        for key, value in arguments.items():
+            if((arg == "--" + key) or (arg == "-" + key[0:1])):
+                arguments[key] = coerce(sys.argv[idx+1])
 
 def coerce(str):
     if(str.lower() == "true"):
