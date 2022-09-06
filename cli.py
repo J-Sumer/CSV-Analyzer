78ca5d2d276844a7d02e6b2835fd367c9dc372a9
Jyothi Sumer <mjyothisumer@gmail.com>
2022-09-05 22:10:40 -0400

    Added coerce function

--- cli.py
+++ cli.py
@@ -4,3 +4,14 @@ for idx, arg in enumerate(sys.argv):
     for key, value in arguments.items():
         if((arg == "--" + key) or (arg == "-" + key[0:1])):
             arguments[key] = coerce(sys.argv[idx+1])
+
+def coerce(str):
+    if(str.lower() == "true"):
+        return True
+    if(str.lower() == "false"):
+        return False
+    try:
+        return int(str)
+    except:
+        pass
+    return str.strip()
