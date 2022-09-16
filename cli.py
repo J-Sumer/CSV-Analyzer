423bae845dbac6237cd09d8e8baad7834b3e81b6
Jyothi Sumer <mjyothisumer@gmail.com>
2022-09-16 18:30:29 -0400

    Moved coerce function to seperate file

--- cli.py
+++ cli.py
@@ -1,18 +1,8 @@
 import sys
+from helperFunctions import *
 
 def updateCli(arguments):
     for idx, arg in enumerate(sys.argv):
         for key, value in arguments.items():
             if((arg == "--" + key) or (arg == "-" + key[0:1])):
                 arguments[key] = coerce(sys.argv[idx+1])
-
-def coerce(str):
-    if(str.lower() == "true"):
-        return True
-    if(str.lower() == "false"):
-        return False
-    try:
-        return int(str)
-    except:
-        pass
-    return str.strip()
